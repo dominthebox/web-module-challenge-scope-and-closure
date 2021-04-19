4f90cf59fd4ae47e193cea9777d523b595d644ac
dominthebox <dsallustro@gmail.com>
2021-04-19 14:19:52 -0400

    answered the first question for task 1

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+  Count is being defined outside of the CounterMaker function in counter2 code, whereas it is defined in teh counterMaker function in counter1 code.
   
   2. Which of the two uses a closure? How can you tell?
   
